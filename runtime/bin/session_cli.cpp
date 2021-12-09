a1eed840ef0de9ee26728367a65695583c175d6c
mcopik <mcopik@gmail.com>
2021-12-09 22:01:43 +0100

    Add signal handlers for SIGTERM and SIGSEGV for session exec

--- runtime/bin/session_cli.cpp
+++ runtime/bin/session_cli.cpp
@@ -4,11 +4,25 @@
 #include <thread>
 #include <climits>
 #include <sys/time.h>
+#include <execinfo.h>
 
 #include <spdlog/spdlog.h>
 
 #include <praas/session.hpp>
 #include "opts.hpp"
+
+void handler(int signum)
+{
+
+  void *array[10];
+  size_t size;
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", signum);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
 
 int main(int argc, char ** argv)
 {
@@ -19,6 +33,16 @@ int main(int argc, char ** argv)
     spdlog::set_level(spdlog::level::info);
   spdlog::set_pattern("[%H:%M:%S:%f] [P %P] [T %t] [%l] %v ");
   spdlog::info("Executing PraaS session executor!");
+
+
+  struct sigaction sa;
+  memset(&sa, 0, sizeof(struct sigaction));
+  sigemptyset(&sa.sa_mask);
+  sa.sa_handler = handler;
+  sa.sa_flags   = SA_SIGINFO;
+
+  sigaction(SIGSEGV, &sa, NULL);
+  sigaction(SIGTERM, &sa, NULL);
 
   // FIXME: parameters
   praas::session::Session server{opts.session_id, 1, 1};
