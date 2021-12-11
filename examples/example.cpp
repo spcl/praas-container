b4945100d0ac3169ea1e70321449b751c47ddbcc
mcopik <mcopik@gmail.com>
2021-12-11 17:14:55 +0100

    Add more shared-memory functions

--- examples/example.cpp
+++ examples/example.cpp
@@ -31,6 +31,17 @@ extern "C" int shm_example(
   int b = std::min(bytes, shm_bytes);
   memcpy(shm, buffer, b);
   std::string msg = std::string{"Written "} + std::to_string(b) + " bytes";
-  out->send(msg.c_str(), msg.length(), function_id);
+  out->send(msg.c_str(), msg.length() + 1, function_id);
+  return 0;
+}
+
+extern "C" int shm_read_example(
+  uint8_t* buffer, size_t, uint8_t* shm, size_t shm_bytes,
+  const std::string& function_id, praas::output::Channel* out
+)
+{
+  size_t bytes_to_read = *reinterpret_cast<int32_t*>(buffer);
+  int bytes_to_send = std::min(bytes_to_read, shm_bytes);
+  out->send(reinterpret_cast<char*>(shm), bytes_to_send, function_id);
   return 0;
 }
