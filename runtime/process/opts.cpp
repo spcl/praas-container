fdaf928bc31c8636fbeac7214e4be6a09a887ab9
mcopik <mcopik@gmail.com>
2021-12-12 20:23:39 +0100

    Update CLI options

--- runtime/process/opts.cpp
+++ runtime/process/opts.cpp
@@ -11,7 +11,7 @@ namespace praas::process {
     options.add_options()
       ("process-id", "Process ID.",  cxxopts::value<std::string>())
       ("control-plane-address", "IP address of control plane.",  cxxopts::value<std::string>())
-      ("hole-puncher-addr", "IP address of hole puncher.",  cxxopts::value<std::string>())
+      ("hole-puncher-address", "IP address of hole puncher.",  cxxopts::value<std::string>())
       ("max-sessions", "Max sessions.", cxxopts::value<int>()->default_value("1"))
       ("enable-swapping", "Enable swapping.", cxxopts::value<bool>()->default_value("true"))
       ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"))
@@ -21,8 +21,8 @@ namespace praas::process {
     Options result;
     result.process_id = parsed_options["process-id"].as<std::string>();
     result.control_plane_address = parsed_options["control-plane-address"].as<std::string>();
-    result.hole_puncher_address = parsed_options["hole-puncher-addr"].as<std::string>();
-    result.max_sessions = parsed_options["max-session"].as<int>();
+    result.hole_puncher_address = parsed_options["hole-puncher-address"].as<std::string>();
+    result.max_sessions = parsed_options["max-sessions"].as<int>();
     result.enable_swapping = parsed_options["enable-swapping"].as<bool>();
     result.verbose = parsed_options["verbose"].as<bool>();
 
